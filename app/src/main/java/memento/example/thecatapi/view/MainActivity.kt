6b266feb699d7e2a215d2f584d6f6cd357dfd129
CristianZ12 <70823679+CristianZ12@users.noreply.github.com>
2021-04-05 21:51:53 -0600

    Part1

--- app/src/main/java/memento/example/thecatapi/view/MainActivity.kt
+++ app/src/main/java/memento/example/thecatapi/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package memento.example.thecatapi
+package memento.example.thecatapi.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import memento.example.thecatapi.R;
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
